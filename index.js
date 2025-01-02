7b401c56a272c852342b47837ee26dcbcd3d67ad
Abdiaziz <145577421+0paziz@users.noreply.github.com>
2025-01-02 17:45:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const result= $("#result");
+result.css("display","none");
+
+const date= new Date();
+const current_year= date.getFullYear();
+const current_month= date.getMonth();
+const current_day= date.getDate();
+
+
+
+
+
+
+
+
+const calculatebtn= $("button");
+
+
+calculatebtn.on("click", function(){
+
+    var User_Birth_date_str = $('input[type="date"]').val();  
+    const User_Birth_date_obj = new Date(User_Birth_date_str);
+    var User_year= User_Birth_date_obj.getFullYear();
+
+    var age=current_year - User_year;
+if(!age){
+    alert("you can't left blank!")
+}
+else{
+    $("#Y_age").text(age);
+    result.css("display","block");
+}
+
+  
+     
+});
+
+
